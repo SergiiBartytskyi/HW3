edaeb6b7de684a30308e221bd268d76f0dca7060
SergiiBartytskyi <108729124+SergiiBartytskyi@users.noreply.github.com>
2024-03-10 19:20:06 +0100

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+var throttle = require('lodash.throttle');
 
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onPlay, 1000));
+isPlayed();
+
+function onPlay({ seconds }) {
+  localStorage.setItem(STORAGE_KEY, seconds);
+}
+
+function isPlayed() {
+  const played = localStorage.getItem(STORAGE_KEY);
+  if (played) {
+    player.setCurrentTime(played);
+  }
+}
